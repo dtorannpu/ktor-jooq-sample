618aa86cd07a97d30dc563ba97206c8001b092ca
dtorannpu <dtorannpu@gmail.com>
2024-06-30 16:00:42 +0900

    kotest追加

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 val kotlinVersion: String by project
 val logbackVersion: String by project
+val kotestVersion: String by project
 
 plugins {
     kotlin("jvm") version "2.0.0"
@@ -27,4 +28,10 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:$logbackVersion")
     testImplementation("io.ktor:ktor-server-tests-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
+    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
+}
+
+tasks.withType<Test>().configureEach {
+    useJUnitPlatform()
 }
